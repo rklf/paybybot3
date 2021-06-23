5331245f00e419411173e6d2d564f527508858a4
Louis Abraham <louis.abraham@yahoo.fr>
2021-06-24 00:12:28 +0200

    use safe yaml load and use .config

--- paybybot2/config.py
+++ paybybot2/config.py
@@ -4,12 +4,12 @@ import logging
 import yaml
 
 
-yml_path = Path("~/.paybybot2.yml").expanduser()
+yml_path = Path("~/.config/paybybot2.yml").expanduser()
 if yml_path.exists():
     with yml_path.open() as ymlfile:
-        config = yaml.load(ymlfile)
+        config = yaml.safe_load(ymlfile)
 else:
-    logging.warning("~/.paybybot2.yml doesn't exist")
+    logging.warning("~/.config/paybybot2.yml doesn't exist")
     config = {}
 
 
