f6bbb1b4fda6b3e36167ceaefb20fd6f68612a73
louisabraham <louis.abraham@yahoo.fr>
2021-06-24 09:59:56 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="paybybot2",
-    version="0.0.1",
+    version="0.0.2",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
