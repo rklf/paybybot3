f8d1982529dcf561c4a6ea9694bdc9ca37fe4ff7
rklf <11515950+rklf@users.noreply.github.com>
2023-10-03 02:24:46 +0200

    Implement Apprise for push notifications

--- paybybot3/__init__.py
+++ paybybot3/__init__.py
@@ -1,9 +1,33 @@
-import logging
+import logging.config
 from os.path import expanduser
 
 
-logging.basicConfig(
-    format="%(asctime)s %(message)s",
-    filename=expanduser("~/paybybot3.log"),
-    level=logging.INFO,
-)
+LOGGER_CONFIG = {
+    'version': 1,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'stream': 'ext://sys.stdout'
+        },
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': expanduser('~/paybybot3.log'),
+            'mode': 'a',
+            'maxBytes': 100_000,
+            'backupCount': 2
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console', 'file']
+    }
+}
+
+logging.config.dictConfig(LOGGER_CONFIG)
