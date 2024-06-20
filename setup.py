85762206b68f1a6abed7ca1b17f4de35878fb2e7
rklf <11515950+rklf@users.noreply.github.com>
2024-06-20 12:55:10 +0200

    Update package from PyPI

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="paybybot3",
-    version="0.1.2",
+    version="0.1.3",
     author="rklf but originally created by Louis Abraham (louisabraham)",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
